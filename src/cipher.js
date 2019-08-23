c8d2e1bc6fe165fb338f1c474ca4a92bf968040c
marielysis <marielys11@yahoo.com>
2019-08-23 18:48:37 -0400

    cifrado terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,25 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+    encode: (encryptMessage, codOffSet) => {
+        /* Acá va tu código */
+        let messageConct = "";
+
+        for (let i = 0; i < encryptMessage.length; i++) {
+            if (encryptMessage.charCodeAt(i) >= 65 && encryptMessage.charCodeAt(i) <= 90) {
+                let resultadoCifrado1 = encryptMessage.charCodeAt(i);
+                let resultadoCifrado11 = ((resultadoCifrado1 - 65 + codOffSet) % 26 + 65);
+                //console.log(resultadoCifrado11);
+                let resultCipher1 = String.fromCharCode(resultadoCifrado11);
+                //console.log(resultCipher1);
+                messageConct += resultCipher1;
+            } else if (encryptMessage.charCodeAt(i) === 32) {
+                let resultadoCifrado11 = encryptMessage.charCodeAt(i);
+                let resultCipher1 = String.fromCharCode(resultadoCifrado11);
+                messageConct += resultCipher1;
+            }
+        }
+        return messageConct;
+    },
+    decode: () => {
+        /* Acá va tu código */
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 /*Acá va tu código*/
+
+let messageConct = "";
+// asigno valor al boton cifrar EVENTO CIFRAR
+const btncifrado = document.getElementById("cifrar")
+btncifrado.addEventListener("click", () => {
+    let codOffSet = Number.parseInt(document.getElementById("offset").value); //asigno el valor del codigo a la variable
+    let encryptMessage = document.getElementById("textocipher").value;
+
+    //Imprimir mensaje cifrado en un nuevo elemento
+    let texarea1 = document.createElement("textarea"); //Primero crear elemnto de tipo texarea
+    let textCipher = document.createTextNode(messageConct); //Crear texto cifrado
+    texarea1.appendChild(textCipher);
+    let contArea = document.getElementById("div1");
+    contArea.appendChild(texarea1) = window.cipher.encode(encryptMessage, codOffSet);
+
+});
